a8748172ce99d4b196e6159cfd13a1c3f15627ae
Sdfabio <48842052+Sdfabio@users.noreply.github.com>
2020-07-09 16:27:45 -0800

    Added files for project 1

---
+++ model_project.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc4_advantage = nn.Linear(fc2_units,action_size)
+        self.fc4_value = nn.Linear(fc2_units,1)
+        
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x_advantage = self.fc4_advantage(x)
+        x_advantage = x_advantage - x_advantage.mean(dim=1, keepdim=True)
+        x_value = self.fc4_value(x)
+        out = x_value + x_advantage
+        return out
